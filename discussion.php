444b4c383af9f05e8f50d4c106f64f817e771568
Kevin Kim <rkdgus990503@gmail.com>
2025-03-18 11:53:29 -0700

    refactor sidebar to be reusable

--- discussion.php
+++ discussion.php
@@ -22,28 +22,7 @@ require_once "php/discussion-function.php";
     <button class="menu-toggle" id="menu-toggle"><i class="fas fa-bars"></i></button>
 
     <div class="container">
-        <aside class="sidebar" id="sidebar">
-            <ul class="menu">
-                <li><a href="#" class="menu-item"><i class="fas fa-briefcase"></i> Jobs</a></li>
-                <li><a href="#" class="menu-item"><i class="fas fa-list"></i> My Jobs List</a></li>
-                <li><a href="discussion.php" class="menu-item active"><i class="fas fa-comments"></i> Discussion</a></li>
-            </ul>
-
-            <div class="settings">
-                <h4>SETTINGS</h4>
-                <a href="settings.php" class="menu-item"><i class="fas fa-cog"></i> Settings</a>
-            </div>
-
-            <div class="user-profile">
-                <div class="avatar-initials">
-                    <?php echo strtoupper($user_name[0]); ?>
-                </div>
-                <div>
-                    <h5><?php echo $user_name; ?></h5>
-                    <p><?php echo $user_email; ?></p>
-                </div>
-            </div>
-        </aside>
+        <?php include "sidebar.php"; ?>
 
         <main class="forum">
             <div class="search-bar">
