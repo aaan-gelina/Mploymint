e0926f494e4a457c9efcae4d1a5268892dbdb486
Kevin Kim <rkdgus990503@gmail.com>
2025-03-18 12:08:09 -0700

    if user logout, session destroy

--- top-navbar.php
+++ top-navbar.php
@@ -7,7 +7,7 @@ $logged_in = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true;
   <h1 class="logo">Mploymint</h1>
   <nav>
     <?php if ($logged_in): ?>
-      <a href="/Mploymint/landing.php" class="login-btn">Logout</a>
+      <a href="/Mploymint/php/logout-function.php" class="login-btn">Logout</a>
     <?php else: ?>
       <a href="/Mploymint/login.php" class="login-btn">Login</a>
     <?php endif; ?>
