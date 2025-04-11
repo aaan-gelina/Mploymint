e0c2794ee47487712c433a59d1c6a454705f8468
Kevin Kim <rkdgus990503@gmail.com>
2025-04-10 20:21:03 -0700

    fixing db for server

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-    $user = 'root';
-    $pass = '';
-    $db = 'mploymintdb';
+    $user = 'kkim9901';
+    $pass = 'kkim9901';
+    $db = 'kkim9901';
 
     $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect.");
 ?>
