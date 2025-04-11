4627ead55fe8690457579f65ea21a29bd6c5ac42
Kevin Kim <rkdgus990503@gmail.com>
2025-04-10 20:52:35 -0700

    trouble shooting

--- php/my_joblist-function.php
+++ php/my_joblist-function.php
@@ -2,7 +2,7 @@
 session_start();
 
 //include 'dbconnect.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/Mploymint/dbconnect.php';
+require($_SERVER['DOCUMENT_ROOT'] . '/Mploymint/dbconnect.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
