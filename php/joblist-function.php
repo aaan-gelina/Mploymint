6f8720975a9fa39615b784b8a7f68715b660c13f
Kevin Kim <rkdgus990503@gmail.com>
2025-04-10 21:13:44 -0700

    fixing path on joblist adding dir

--- php/joblist-function.php
+++ php/joblist-function.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
+
+include __DIR__ . '/../dbconnect.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include 'dbconnect.php';
-
 $uid = $_SESSION["uid"];
 
 $query = "SELECT name, email, type FROM user WHERE uid = ?";
