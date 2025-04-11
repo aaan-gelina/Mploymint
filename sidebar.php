68c768c1d183d3643abe9d6c30c3b41cf85d8b44
Kevin Kim <rkdgus990503@gmail.com>
2025-04-10 17:54:57 -0700

    added redirection to admin

--- sidebar.php
+++ sidebar.php
@@ -23,17 +23,22 @@ $is_default_img = $profileimg === 'profile.jpg';
 <link rel="stylesheet" href="css/sidebar.css">
 <aside class="sidebar" id="sidebar">
 <ul class="menu">
-    <?php if ($_SESSION['type'] === 'company'): ?>
-      <li><a href="./joblist.php?type=company" class="menu-item"><i class="fas fa-briefcase"></i> Jobs</a></li>
-      <li><a href="./applicant_list.php" class="menu-item"><i class="fas fa-users"></i> Applicant List</a></li>
-      <li><a href="./profile.php" class="menu-item <?php echo $current_page === 'profile.php' ? 'active' : ''; ?>"><i class="fas fa-user"></i> Profile</a></li>
-    <?php else: ?>
-      <li><a href="./joblist.php" class="menu-item"><i class="fas fa-briefcase"></i> Jobs</a></li>
-      <li><a href="./my_joblist.php" class="menu-item"><i class="fas fa-list"></i> My Jobs List</a></li>
-      <li><a href="./discussion.php" class="menu-item"><i class="fas fa-comments"></i> Discussion</a></li>
-      <li><a href="./profile.php" class="menu-item <?php echo $current_page === 'profile.php' ? 'active' : ''; ?>"><i class="fas fa-user"></i> Profile</a></li>
-    <?php endif; ?>
-  </ul>
+  <?php if ($_SESSION['type'] === 'company'): ?>
+    <li><a href="./joblist.php?type=company" class="menu-item"><i class="fas fa-briefcase"></i> Jobs</a></li>
+    <li><a href="./applicant_list.php" class="menu-item"><i class="fas fa-users"></i> Applicant List</a></li>
+    <li><a href="./profile.php" class="menu-item <?php echo $current_page === 'profile.php' ? 'active' : ''; ?>"><i class="fas fa-user"></i> Profile</a></li>
+
+  <?php else: ?>
+    <li><a href="./joblist.php" class="menu-item"><i class="fas fa-briefcase"></i> Jobs</a></li>
+    <li><a href="./my_joblist.php" class="menu-item"><i class="fas fa-list"></i> My Jobs List</a></li>
+    <li><a href="./discussion.php" class="menu-item"><i class="fas fa-comments"></i> Discussion</a></li>
+    <li><a href="./profile.php" class="menu-item <?php echo $current_page === 'profile.php' ? 'active' : ''; ?>"><i class="fas fa-user"></i> Profile</a></li>
+  <?php endif; ?>
+
+  <?php if ($_SESSION['type'] === 'admin'): ?>
+    <li><a href="./admin.php" class="menu-item"><i class="fas fa-tools"></i> Admin</a></li>
+  <?php endif; ?>
+</ul>
 
   <div class="settings">
     <h4>SETTINGS</h4>
