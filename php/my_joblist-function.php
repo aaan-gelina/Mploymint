a555ca4072f761bace455fb05d72220fc97a1376
Kevin Kim <rkdgus990503@gmail.com>
2025-04-10 20:40:23 -0700

    Trying different path

--- php/my_joblist-function.php
+++ php/my_joblist-function.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 
-include 'dbconnect.php';
+//include 'dbconnect.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/Mploymint/dbconnect.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
